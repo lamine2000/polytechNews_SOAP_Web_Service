44dfe135770b07a98e2407b396a499b212032463
lamine2000 <lemzogueye.lg72@gmail.com>
2022-08-04 04:13:23 +0000

    making the service accessible from a distant machine

--- src/server/PolytechNews.java
+++ src/server/PolytechNews.java
@@ -5,7 +5,7 @@ import javax.xml.ws.Endpoint;
 
 public class PolytechNews {
     public static void main(String[] args) {
-        String url = "http://localhost:1122/?wsdl";
+        String url = "http://194.163.171.45:1122/?wsdl";
         Endpoint.publish(url, new UserManager());
         System.out.println("Webservice is available on " +url);
     }
